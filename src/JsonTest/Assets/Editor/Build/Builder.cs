a792fe937aefe9edfc9ef863c446948f203d367c
Leonid Vasilev <savidiy@mail.ru>
2021-07-02 16:05:38 +0300

    fail exception

--- src/JsonTest/Assets/Editor/Build/Builder.cs
+++ src/JsonTest/Assets/Editor/Build/Builder.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
+using static UnityEditor.BuildPipeline;
 
 namespace Editor.Build
 {
@@ -7,13 +10,16 @@ namespace Editor.Build
         [MenuItem("Build/Android")]
         public static void MyBuild()
         {
-            BuildPipeline.BuildPlayer(new BuildPlayerOptions()
+            BuildReport report = BuildPlayer(new BuildPlayerOptions()
             {
                 target = BuildTarget.Android,
                 scenes = new[] {"Assets/Scenes/SampleScene.unity"},
                 locationPathName = "../../artifacts/game.apk",
                 options = BuildOptions.None,
             });
+
+            if (report.summary.result != BuildResult.Succeeded)
+                throw new Exception("Failed build Android. See log for details.");
         }
     }
 }
